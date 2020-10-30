525a6e01119385fb0d40da8e5e8128ffbd731547
EricJ <ericjardon@hotmail.com>
2020-10-30 14:04:21 -0600

    Integrated Inventory Assets to Scene. Added Inventory UI to Canvas

---
+++ Assets/Scripts/Game/Inventory/HealthPotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+    private Transform player;
+    // Start is called before the first frame update
+    private void Start(){
+    
+    player= GameObject.FindGameObjectWithTag("Player").transform;
+}
+
+     public void TeleportPlayerStart(){
+
+     player.position= new Vector2(0,0);
+     Destroy(gameObject);      
+
+     }
+}
+//Test
