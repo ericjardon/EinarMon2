cf05fe18fcee3c48aa921bb49f9ce2633db4c0b7
EricJ <ericjardon@hotmail.com>
2020-11-09 17:35:26 -0600

    Second Delivery, Complete Rival Battle Scene

---
+++ Assets/Scripts/Game/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioClip TownSong, BattleSong, DefeatSound;
+
+    public static AudioSource audioSource;
+    
+    void Start()
+    {
+        /*TownSong= Resources.Load<AudioClip>("townThene");
+        Food= Resources.Load<AudioClip>("battleVs");
+
+        audioSource= GetComponent<AudioSource>();*/
+    }
+
+
+
+}
