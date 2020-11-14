bb2dd632fe7272ec86d0f806437f59c64d2bd223
Bryan Monroy <a01026848@itesm.mx>
2020-11-14 11:42:42 -0600

    Gym Scene

---
+++ Assets/Scripts/Game/Teleportations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportations : MonoBehaviour
+{
+    public GameObject gym;
+    private Vector2 posGym;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posGym = gym.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        col.gameObject.transform.position= new Vector2(posGym.x,posGym.y);
+    }
+}
